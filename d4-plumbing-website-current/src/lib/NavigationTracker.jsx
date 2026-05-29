c730e9834dbfeeca6785db3205bab50e462f03ae
Creative Cowboys <login@creativecowboys.co>
2026-05-29 15:20:29 -0400

    fix: Reset scroll position to top on navigation/page transition

--- d4-plumbing-website-current/src/lib/NavigationTracker.jsx
+++ d4-plumbing-website-current/src/lib/NavigationTracker.jsx
@@ -10,6 +10,11 @@ export default function NavigationTracker() {
     const { Pages, mainPage } = pagesConfig;
     const mainPageKey = mainPage ?? Object.keys(Pages)[0];
 
+    // Reset scroll position to top when navigating to a new route
+    useEffect(() => {
+        window.scrollTo(0, 0);
+    }, [location.pathname]);
+
     // Log user activity when navigating to a page
     useEffect(() => {
         // Extract page name from pathname
